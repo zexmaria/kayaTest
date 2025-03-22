dcccdaddf52f82d5490e9a4c16b3e465e3bd463c
ZéMaria <duartemaf@gmail.com>
2025-03-22 17:29:29 -0300

    final frontend and dinamic populate

--- core/models.py
+++ core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    nome = models.CharField(max_length=200)
+    especialidade = models.CharField(max_length=200)
+    crm = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    visualizacoes = models.IntegerField(default=0)
+
+    descricao = models.TextField(
+        help_text="Descreva a pessoa do médico."
+    )
+
+    patologias = models.TextField(
+        help_text="Lista de patologias, separadas por vírgula."
+    )
+
+    atendimento = models.TextField(
+        help_text="Grupos atendidos (ex: Crianças, Adolescentes, Adultos e Idosos)."
+    )
+
+    convenio = models.BooleanField(default=False)
+
+    retorno_acompanhamento = models.CharField(
+        max_length=100,
+        blank=True, null=True,
+        help_text="Ex: 'Retorno em até 30 dias'"
+    )
+
+    experiencia_prescricao = models.CharField(
+        max_length=500,
+        blank=True, null=True,
+        help_text="Ex: 'Sim' ou 'Não' ou 'Não informado'"
+    )
+
+    formacao = models.TextField(
+        blank=True, null=True,
+        help_text="Onde se formou e suas especialidades"
+    )
+
+    foto = models.ImageField(upload_to='doctors/', blank=True, null=True)
+
+    preco_consulta = models.DecimalField(max_digits=8, decimal_places=2, default="00.00")
+
+    duracao_consulta = models.IntegerField(help_text="Duração da consulta em minutos", default='00')
+
+    def __str__(self):
+        return self.nome
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Doctor
 
-# Create your views here.
+def doctor_profile(request, doctor_id):
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    return render(request, 'core/doctor_profile.html', {'doctor': doctor})
+
+def doctor(request):
+    doctor = Doctor.objects.all()
+    return render(request, 'index.html', {'medicos': doctor})
+
+def home(request):
+    doctors = Doctor.objects.all()
+    return render(request, 'core/index.html', {'doctors': doctors})
